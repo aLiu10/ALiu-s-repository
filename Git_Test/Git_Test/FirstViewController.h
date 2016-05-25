1ae81aeddcf0e48f57aef13c34ac6d140c3e0062
Aliu <1072492601@qq.com>
2016-05-25 15:57:08 +0800

    新建了一个controller 新定义了一个string

---
+++ Git_Test/Git_Test/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Git_Test
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 Aliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
