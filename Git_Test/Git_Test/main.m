88954389a6bcd940ad127dd88c91ae6ea1189dba
Aliu <1072492601@qq.com>
2016-05-25 15:50:46 +0800

    create a  new test project

---
+++ Git_Test/Git_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git_Test
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 Aliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
